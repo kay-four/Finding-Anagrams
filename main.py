b4337007ae2d1208c85d231f8e73b63a8538a26e
kay-four <88488188+kay-four@users.noreply.github.com>
2022-05-25 18:39:32 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if(len(word) != len(anagram)):
+        return print(False)
+    return print((sorted(word)==sorted(anagram)))
+    
+word = input("Enter a word: ")
+anagram = input("Enter another word:")
+
+find_anagram(word,anagram)
